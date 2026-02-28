30d0dca74607d16b823c2a6d3641f7bdd170f4da
Anna Rodina <tavoshka@gmail.com>
2026-02-28 21:05:33 +0200

    fixed

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -6,9 +6,7 @@ export const PostInfo = ({ post }) => (
   <div className="PostInfo">
     <div className="PostInfo__header">
       <h3 className="PostInfo__title">{post.title}</h3>
-      <p>
-        Posted by <UserInfo user={post.user} />
-      </p>
+      <p>Posted by {post.user && <UserInfo user={post.user} />}</p>
     </div>
 
     <p className="PostInfo__body">{post.body}</p>
